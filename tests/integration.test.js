21dcaf2d24a381c49028c77878900f31d266cebb
Maksym Fedan <maksym.fedan@gmail.com>
2020-08-10 23:27:24 +0300

    added mock for mongoose and some kind of integration tests. Total time spent: ~7h

--- tests/integration.test.js
+++ tests/integration.test.js
@@ -1,27 +1,59 @@
 const request = require('supertest');
 const app = require('../src/app');
+const mockingoose = require('mockingoose').default;
+const Record = require('../src/models/Record');
 
-const SUCCESS_RESPONSE_MSG = 'Success';
+const { RESPONSE_MSG, RESPONSE_CODE } = require('../src/constants');
+
+const MOCK_RECORDS = [{
+  "key": "AWRWERWED23423",
+  "createdAt": "2020-08-10T08:48:35.889Z",
+  "counts": [10, 20, 10]
+},
+{
+  "key": "QQQQERWED23423",
+  "createdAt": "2020-08-10T08:49:27.387Z",
+  "counts": [10, 5, 10]
+},
+{
+  "key": "AASDQQZXCZXCRWED23423",
+  "createdAt": "2020-08-10T08:49:49.293Z",
+  "counts": [10, 50, 10]
+}];
+
+const BASIC_PARAMS_EXAMPLE = {
+  startDate: "2016-01-26",
+  endDate: "2020-12-30"
+};
+
+mockingoose.Record.toReturn(MOCK_RECORDS, 'aggregate');
 
 describe('POST /', function () {
-  it('responds with json', async (done) => {
-    request(app)
+  it('responds with json', function () {
+    return request(app)
       .post('/')
-      .expect('Content-Type', /json/)
-      .expect(200)
-      .end(function (err, res) {
-        if (err) throw err;
-        done();
-      });
+      .send(BASIC_PARAMS_EXAMPLE)
+      .expect('Content-Type', /json/);
   });
 
-  // Assuming that no params should return all the records
-  it('has a proper structure response', async (done) => {
-    const response = await request(app).post('/');
-    expect(response.code).toBeGreaterThanOrEqual(0);
-    expect(response.msg).toMatch(SUCCESS_RESPONSE_MSG);
-    expect(response.records).toEqual(expect.any(Array));
-    done();
+  it('has a proper structure response', () => {
+    return request(app).post('/').send(BASIC_PARAMS_EXAMPLE).then((response) => {
+      const { body } = response;
+      expect(body.code).toEqual(RESPONSE_CODE.SUCCESS);
+      expect(body.msg).toMatch(RESPONSE_MSG.SUCCESS);
+      expect(body.records).toEqual(expect.any(Array));
+    });
+  });
+
+  it('responses with error when no mandatory params filled', () => {
+    return request(app).post('/')
+      .expect('Content-Type', /json/)
+      .then((response) => {
+        const { body } = response;
+        expect(body.code).toEqual(RESPONSE_CODE.ERROR);
+        expect(body.msg).toMatch(RESPONSE_MSG.MISSED_REQUIRED_PARAM);
+        expect(body.records).toEqual(expect.any(Array));
+      });
   });
 });
 
